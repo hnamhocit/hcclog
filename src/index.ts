d92a4b0b3a00b8c2ae2a0251740075633f7a758b
hnamhocit <hnamhocit@gmail.com>
2025-04-17 16:27:57 +0700

    fix strip-ansi

--- src/index.ts
+++ src/index.ts
@@ -1,142 +1,135 @@
-import * as pc from "picocolors";
-import * as fs from "fs";
-import stripAnsi from "strip-ansi";
+import * as fs from 'fs'
+import * as pc from 'picocolors'
+import stripAnsi from 'strip-ansi' // Correct import
 
 enum LEVELS {
-  DEBUG = "DEBUG",
-  INFO = "INFO",
-  WARN = "WARN",
-  ERROR = "ERROR",
-  LOG = "LOG",
+	DEBUG = 'DEBUG',
+	INFO = 'INFO',
+	WARN = 'WARN',
+	ERROR = 'ERROR',
+	LOG = 'LOG',
 }
 
 interface Config {
-  logFile?: boolean;
-  logFileName?: string;
-  bracket?: boolean;
-  color?: boolean;
-  timestamp?: boolean;
-  localTimestamp?: boolean;
+	logFile?: boolean
+	logFileName?: string
+	bracket?: boolean
+	color?: boolean
+	timestamp?: boolean
+	localTimestamp?: boolean
 }
 
 class Logger {
-  private config: Config;
+	private config: Config
 
-  constructor(config?: Config) {
-    this.config = {
-      logFileName: "app.log",
-      logFile: false,
-      bracket: false,
-      color: true,
-      timestamp: true,
-      localTimestamp: false,
-      ...config,
-    };
+	constructor(config?: Config) {
+		this.config = {
+			logFileName: 'app.log',
+			logFile: false,
+			bracket: false,
+			color: true,
+			timestamp: true,
+			localTimestamp: false,
+			...config,
+		}
 
-    if (!fs.existsSync(this.config.logFileName as string)) {
-      fs.writeFileSync(this.config.logFileName as string, "");
-    }
-  }
+		if (!fs.existsSync(this.config.logFileName as string)) {
+			fs.writeFileSync(this.config.logFileName as string, '')
+		}
+	}
 
-  private getTimestamp(): string {
-    return this.config.timestamp
-      ? this.config.localTimestamp
-        ? new Date().toLocaleString() + " "
-        : new Date().toISOString() + " "
-      : "";
-  }
+	private getTimestamp(): string {
+		return this.config.timestamp
+			? this.config.localTimestamp
+				? new Date().toLocaleString() + ' '
+				: new Date().toISOString() + ' '
+			: ''
+	}
 
-  private formatMessage(level: LEVELS, message: string): string {
-    switch (level) {
-      case LEVELS.DEBUG:
-        return pc.magenta(message);
-      case LEVELS.INFO:
-        return pc.blue(message);
-      case LEVELS.WARN:
-        return pc.yellow(message);
-      case LEVELS.ERROR:
-        return pc.red(message);
-      case LEVELS.LOG:
-        return pc.green(message);
-    }
-  }
+	private formatMessage(level: LEVELS, message: string): string {
+		switch (level) {
+			case LEVELS.DEBUG:
+				return pc.magenta(message)
+			case LEVELS.INFO:
+				return pc.blue(message)
+			case LEVELS.WARN:
+				return pc.yellow(message)
+			case LEVELS.ERROR:
+				return pc.red(message)
+			case LEVELS.LOG:
+				return pc.green(message)
+		}
+	}
 
-  private formatTimestamp(): string {
-    return pc.dim(pc.italic(this.getTimestamp()));
-  }
+	private formatTimestamp(): string {
+		return pc.dim(pc.italic(this.getTimestamp()))
+	}
 
-  private formatLevel(level: LEVELS): string {
-    let formatted = this.config.bracket ? `[${level}]` : ` ${level} `;
-    switch (level) {
-      case LEVELS.DEBUG:
-        return pc.bgMagenta(pc.black(formatted));
-      case LEVELS.INFO:
-        return pc.bgBlue(pc.black(formatted));
-      case LEVELS.WARN:
-        return pc.bgYellow(pc.black(formatted));
-      case LEVELS.ERROR:
-        return pc.bgRed(pc.black(formatted));
-      case LEVELS.LOG:
-        return pc.bgGreen(pc.black(formatted));
-    }
-  }
+	private formatLevel(level: LEVELS): string {
+		let formatted = this.config.bracket ? `[${level}]` : ` ${level} `
+		switch (level) {
+			case LEVELS.DEBUG:
+				return pc.bgMagenta(pc.black(formatted))
+			case LEVELS.INFO:
+				return pc.bgBlue(pc.black(formatted))
+			case LEVELS.WARN:
+				return pc.bgYellow(pc.black(formatted))
+			case LEVELS.ERROR:
+				return pc.bgRed(pc.black(formatted))
+			case LEVELS.LOG:
+				return pc.bgGreen(pc.black(formatted))
+		}
+	}
 
-  private formatString(level: LEVELS, message: any[]) {
-    const timestampStyle = this.formatTimestamp();
-    const levelStyle = this.formatLevel(level);
+	private formatString(level: LEVELS, message: any[]) {
+		const timestampStyle = this.formatTimestamp()
+		const levelStyle = this.formatLevel(level)
 
-    let msg = "";
-    for (const item of message) {
-      if (typeof item === "object") {
-        msg += JSON.stringify(item, null, "\t") + " ";
-      } else {
-        msg += item + " ";
-      }
-    }
+		let msg = ''
+		for (const item of message) {
+			if (typeof item === 'object') {
+				msg += JSON.stringify(item, null) + ' '
+			} else {
+				msg += item + ' '
+			}
+		}
 
-    let messageStyle = this.formatMessage(level, msg);
-    let result = `${timestampStyle}${levelStyle} ${messageStyle}`;
+		let messageStyle = this.formatMessage(level, msg)
+		let result = `${timestampStyle}${levelStyle} ${messageStyle}`
 
-    if (this.config.logFile) {
-      this.writeToLogFile(result + "\n");
-    }
+		if (this.config.logFile) {
+			this.writeToLogFile(result + '\n')
+		}
 
-    console.log(result);
-  }
+		console.log(result)
+	}
 
-  private writeToLogFile(content: string): void {
-    let cleanContent = stripAnsi(content);
-    fs.appendFileSync(this.config.logFileName as string, cleanContent, {
-      encoding: "utf8",
-    });
-  }
+	private writeToLogFile(content: string): void {
+		let cleanContent = stripAnsi(content) // Use stripAnsi directly
+		fs.appendFileSync(this.config.logFileName as string, cleanContent, {
+			encoding: 'utf8',
+		})
+	}
 
-  log(...message: any) {
-    this.formatString(LEVELS.LOG, message);
-  }
+	log(...message: any) {
+		this.formatString(LEVELS.LOG, message)
+	}
 
-  info(...message: any) {
-    this.formatString(LEVELS.INFO, message);
-  }
+	info(...message: any) {
+		this.formatString(LEVELS.INFO, message)
+	}
 
-  warn(...message: any) {
-    this.formatString(LEVELS.WARN, message);
-  }
+	warn(...message: any) {
+		this.formatString(LEVELS.WARN, message)
+	}
 
-  error(...message: any) {
-    this.formatString(LEVELS.ERROR, message);
-  }
+	error(...message: any) {
+		this.formatString(LEVELS.ERROR, message)
+	}
 
-  debug(...message: any) {
-    this.formatString(LEVELS.DEBUG, message);
-  }
+	debug(...message: any) {
+		this.formatString(LEVELS.DEBUG, message)
+	}
 }
 
-const logger = new Logger();
-logger.log("this is a log");
-logger.info("this is an info");
-logger.warn("this is a warn");
-logger.error("this is an error");
-logger.debug("this is a debug", { key: "value" }, [2, 3, 4]);
-
-export default Logger;
+export default Logger
